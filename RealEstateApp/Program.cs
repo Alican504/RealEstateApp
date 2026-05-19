d7bb5f6f9ada0e5dafc17af91782776b0b8bbe99
Alican504 <alicaninan2005@gmail.com>
2026-05-19 21:02:47 +0300

    Ufak bir düzeltme

--- RealEstateApp/Program.cs
+++ RealEstateApp/Program.cs
@@ -1,28 +1,22 @@
 using Microsoft.EntityFrameworkCore;
-using RealEstateApp.DAL; // DbContext'imizin bulunduðu katman
+using RealEstateApp.DAL; 
 using RealEstateApp.Service;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 
-// --- BÝZÝM EKLEDÝÐÝMÝZ VERÝTABANI BAÐLANTI AYARI ---
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-// ---------------------------------------------------
 
 builder.Services.AddScoped<IPropertyService, PropertyService>();
 
 builder.Services.AddSession();
-// DÝKKAT: Üstteki ayar mutlaka bu Build() satýrýndan önce olmalý!
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
